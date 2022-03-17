96973503f5e39dd9eaa5995ed5d361d0cc0fdd79
eyannohmmoja <[eyannohmmoja@gmail.com]>
2022-03-16 17:20:26 -0700

    my commit message

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,15 @@
 #include "main.h"
 
 /**
- * print_alphabet - prints the alphabet
- * followed by a new line
+ * print_alphabet - Entry point
+ *
  * Return: void
  */
 
 void print_alphabet(void)
 {
-	int x;
 	char c = 'a';
 
-	for (x =  97; x <= 122; x++)
-		_putchar(x);
 	while (c <= 'z')
 	{
 		_putchar(c);
