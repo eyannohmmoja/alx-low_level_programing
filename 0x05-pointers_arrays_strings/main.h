8e617589c344637f09f9aebc436c3569b4d12b04
eyannohmmoja <[eyannohmmoja@gmail.com]>
2022-03-23 18:22:49 -0700

    swap

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,9 +1,5 @@
-#undef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
-#include <unistd.h>
-
+#ifndef main_h
+#define main_h
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -15,7 +11,4 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
-
 #endif
-
